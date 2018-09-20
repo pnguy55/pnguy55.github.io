0984d9ec14d1237d9e258c09bd36f80d542b95e4
Pnguy55 <pnguy55@gmail.com>
2018-09-19 21:19:29 -0500

    V2.1.2 Fix skills sidebar.

--- js/index.js
+++ js/index.js
@@ -1,32 +1,32 @@
 function menuToggleOn(){
-  
+
   var menu = document.getElementById('burger');
   var button = document.getElementById('clicky');
   var menuPane = document.getElementById('mobile-nav');
   var menuPaneList = document.getElementById('mobile-menu');
- 
+
   function stateChange(newState) {
     setTimeout(function () {
         if (newState == -1) {
             menuPaneList.classList.add('mobile-menu-hide');
         }
-       
+
     }, 650);
   }
-  
 
-  
+
+
   if(menu.classList.contains('menuToggle')){
     button.classList.remove('animButton');
     button.classList.add('anim2Button');
     menu.classList.remove('menuToggle');
     menu.classList.add('menuToggle2');
-    menuPane.classList.remove('mobile-nav');  
+    menuPane.classList.remove('mobile-nav');
     menuPaneList.classList.remove('menuAnim');
     menuPaneList.classList.add('menuAnim2');
     stateChange(-1);
   }
-  
+
   else{
     button.classList.remove('anim2Button');
     button.classList.add('animButton');
@@ -37,10 +37,10 @@ function menuToggleOn(){
     menuPaneList.classList.add('menuAnim');
     menuPaneList.classList.remove('menuAnim2');
     menuPaneList.classList.remove('mobile-menu-hide');
-    
+
   }
-  
-  
+
+
 }
 
 function goToPortfolio(){
@@ -50,26 +50,26 @@ function goToPortfolio(){
   var skills = document.getElementById('skills');
   var portfolioCopy = document.getElementById('portfolio-copy');
   var aside = document.getElementById('aside');
-  
+
   selfie.classList.add('hide');
   aside.classList.add('hide');
   about.classList.add('hide');
-  
+
   portfolio.classList.remove('hide');
   portfolioCopy.classList.remove('hide');
-  
+
 }
 
 function goToAboutMe(){
   var selfie = document.getElementById('selfie-box');
   var about = document.getElementById('about-me');
   var portfolio = document.getElementById('portfolio');
-  var skills = document.getElementById('skills');
+  var aside = document.getElementById('aside');
   var portfolioCopy = document.getElementById('portfolio-copy');
-  
+
   portfolio.classList.add('hide');
   portfolioCopy.classList.add('hide');
   selfie.classList.remove('hide');
-  skills.classList.remove('hide');
+  aside.classList.remove('hide');
   about.classList.remove('hide');
 }
